41d93929e0c49adad5ac8e7e50b9726acd990f31
Lorrainnn <jixual3@uci.edu>
2025-03-27 17:59:45 -0700

    check on secrecy

--- src/login/RecaptchaConstants.java
+++ src/login/RecaptchaConstants.java
@@ -1,8 +1,7 @@
 package login;
 
 public class RecaptchaConstants {
-    //6Lc0U88qAAAAAHYMXFvXNMfoSTb9-Zlrocnmo-I8
 
-    public static final String SECRET_KEY ="6Lc0U88qAAAAAHYMXFvXNMfoSTb9-Zlrocnmo-I8";
+    public static final String SECRET_KEY ="";
 
 }
